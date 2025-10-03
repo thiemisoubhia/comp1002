f16610c526050f905af4110efb6f99dd28bbf3d2
Thiemi Soubhia Doi <99260862+thiemisoubhia@users.noreply.github.com>
2025-10-03 12:13:00 -0400

    lab5 production

--- lesson-4/process-form.php
+++ lesson-4/process-form.php
@@ -16,19 +16,23 @@
     is in the correct format. Security checks must also be made before database queries are made */
 
 	// STEP 16: Set a variable to capture the first name from the submitted form, using the GET array and the fname value
-	
+	$fname = $_GET["fname"];
+
 	// STEP 18: Set a variable to capture the last name from the submitted form, using the GET array and the lname value
-	
+	$lname = $_GET["lname"];
+
 	// STEP 20: Capture the Alert Settings from the submitted form, using the GET array and the alert value
-	
+	$alert = $_GET["alert"];
 	// STEP 17: Output a friendly message to confirm that everything went well, including the $fname variable in a paragraph
-	
+	echo('<p>Thank you, '.$fname. ' ' .$lname.'!</p>');
 	// STEP 19: Update the above paragraph to now include the last name
 
 	echo('<p>The Alert Settings you have chosen include the following notifications:</p>
 	<ul>');
 		// STEP 21: The checkboxes for the Alert Settings will be sent as an array (as there could be more than one option checked by the user - so we need to loop through each item with a FOREACH loop)
-		
+		foreach($alert as $item){
+			echo('<li>'.$item.'</li>');
+		}
 
 
 	echo('</ul>');

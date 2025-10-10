a6b5e4aed933d26aec42ff33332744136c50a577
Thiemi Soubhia Doi <99260862+thiemisoubhia@users.noreply.github.com>
2025-10-10 12:38:51 -0400

    update

---
+++ lesson-6/includes/nav.php
@@ -0,0 +1,8 @@
+<nav>
+    <ul>
+        <li><a href="lesson-6/index.php">Home/a></li>
+        <li><a href="lesson-6/red.php">Red Blocks</a></li>
+        <li><a href="lesson-6/blue.php">Blue Blocks</a></li>
+        <li><a href="lesson-6/yellow.php">Yellow Blocks</a></li>
+    </ul>
+</nav>

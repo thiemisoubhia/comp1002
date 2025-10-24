62644bb65ea3cea5d5b562c127e7f79fdc0ce72f
Thiemi Soubhia Doi <99260862+thiemisoubhia@users.noreply.github.com>
2025-10-24 17:09:13 -0400

    css

--- assignment-1/process-form.php
+++ assignment-1/process-form.php
@@ -1,12 +1,16 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="utf-8" />
     <title>Order Confirmation - The HTML Pizza Company</title>
-    <link rel="stylesheet" href="styles.css" />
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
 </head>
+
 <body>
-   <header>
+    <header>
         <nav>
             <div class="container">
                 <img src="images/logo.png" alt="The HTML Pizza Company logo" width="140">
@@ -32,42 +36,43 @@
     </header>
     <main>
         <h2>Your Order Has Been Received!</h2>
-<?php
-   
-    $fname = $_GET["first-name"];
-    $lname = $_GET["last-name"];
-    $email = $_GET["email"];
-    $phone = $_GET["phone"];
+        <?php
 
-    $address = $_GET["address"];
-    $postal = $_GET["postalcode"];
-    $preference = $_GET["dinning-preference"];
+        $fname = $_GET["first-name"];
+        $lname = $_GET["last-name"];
+        $email = $_GET["email"];
+        $phone = $_GET["phone"];
 
-
-    $message = $_GET["messsage"];
-
-    echo('<p>Thank you, '.$fname.' '.$lname.'!</p>');
-    echo('<p>Your order has been successfully received.</p>');
-
-    echo('<h3>Delivery Details:</h3>');
-    echo('<ul>');
-        echo('<li>Email: '.$email.'</li>');
-        echo('<li>Phone: '.$phone.'</li>');
-        echo('<li>Address: '.$address.'</li>');
-        echo('<li>Postal Code: '.$postal.'</li>');
-    echo('</ul>');
+        $address = $_GET["address"];
+        $postal = $_GET["postalcode"];
+        $preference = $_GET["dinning-preference"];
 
 
-    if (!empty($message)) {
-        echo('<h3>Your Message:</h3>');
-        echo('<p>'.$message.'</p>');
-    } else {
-        echo('<p>No additional message provided.</p>');
-    }
-?>
+        $message = $_GET["messsage"];
+
+        echo ('<p>Thank you, ' . $fname . ' ' . $lname . '!</p>');
+        echo ('<p>Your order has been successfully received.</p>');
+
+        echo ('<h3>Delivery Details:</h3>');
+        echo ('<ul>');
+        echo ('<li>Email: ' . $email . '</li>');
+        echo ('<li>Phone: ' . $phone . '</li>');
+        echo ('<li>Address: ' . $address . '</li>');
+        echo ('<li>Postal Code: ' . $postal . '</li>');
+        echo ('</ul>');
+
+
+        if (!empty($message)) {
+            echo ('<h3>Your Message:</h3>');
+            echo ('<p>' . $message . '</p>');
+        } else {
+            echo ('<p>No additional message provided.</p>');
+        }
+        ?>
     </main>
     <footer>
         <p><small>© The HTML Pizza Company. All rights reserved.</small></p>
     </footer>
 </body>
+
 </html>
